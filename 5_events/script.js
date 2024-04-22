52e26ca5b913b5dd00c5a953d98b04e21cce18eb
Caroline <deconinck.caro@gmail.com>
2024-04-22 11:57:23 +0200

    Added exercise 5

---
+++ 5_events/script.js
@@ -0,0 +1,71 @@
+const _initTime = Date.now()
+
+const getElapsedTime = () => {
+    return Number((Date.now() - _initTime) / 1000).toFixed(2) + 's'
+}
+
+// Create a new square with event listener
+const createSquare = (newClass) => {
+    const destination = document.querySelector(".displayedsquare-wrapper")
+    const newSquare = document.createElement("div");
+    newSquare.classList.add("displayedsquare", newClass);
+    newSquare.addEventListener("click", (event) => {
+        alert(newClass);
+    })
+    destination.appendChild(newSquare);
+}
+
+const deleteSquares = ()  => {
+    const newSquares = document.querySelectorAll(".displayedsquare");
+    for (square of newSquares) {
+        square.remove();
+    }
+}
+
+// Adds log each time an action is performed
+const editLog = (target) => {
+    const ul = document.querySelector("ul");
+    const li = document.createElement("li");
+    if (target === "spacebar") {
+        li.innerText = `[${getElapsedTime()}] Spacebar was clicked!`
+    }
+    else if (target === "l") {
+        while (ul.firstChild) {
+            ul.firstChild.remove();
+        }
+        return 0;
+    }
+    else {
+        li.innerText = `[${getElapsedTime()}] Created a new ${target.classList[1]} square.`
+    }
+    ul.appendChild(li);
+}
+
+const clickOnSquare = (e) => {
+    createSquare(e.target.classList[1]);
+    editLog(e.target);
+}
+
+// Changes body background color
+const body = document.querySelector("body");
+body.addEventListener("keypress", (event) => {
+    // change body background color if spacebar pressed
+    if (event.key === " ") {
+        let randomColor = `rgb(${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)}`
+        body.style.backgroundColor = randomColor;
+        editLog("spacebar");
+    }
+    // delete all new squares when s is pressed
+    if (event.key === "s") {
+        deleteSquares();
+    }
+    // Modifies logs when l is pressed
+    if (event.key === "l") {
+        editLog("l");
+    }
+})
+
+const actionSquares = document.querySelectorAll('.actionsquare');
+for (let actionSquare of actionSquares) {
+    actionSquare.addEventListener('click', clickOnSquare)
+}
